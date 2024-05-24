784b3f4e047ff93f007576053174651aba0b3e35
hyeon-woo1 <gusdn4969@gmail.com>
2024-05-24 17:15:00 +0900

    {test1}

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState , useEffect, useSyncExternalStore} from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState([]);
@@ -15,29 +16,34 @@ function Home() {
   
     };
   
-    
+
     useEffect(()=> {
       getMovies();
       console.log(movies);
     } , []);
     return (
       <div>
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie 
-              key = {movie.id}
-              id = {movie.id}
-              coverImg={movie.medium_cover_image} 
-              title={movie.title} 
-              summary={movie.summary} 
-              genres={movie.genres} 
-            />
-          ))}
+        <div className={styles.container}>
+          {loading ? (
+            <div className={styles.loader}>
+              <span>Loading...</span>
+            </div>
+          ) : (
+            <div className={styles.movies}>
+            {movies.map((movie) => (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id}
+                year={movie.year}
+                coverImg={movie.medium_cover_image} 
+                title={movie.title} 
+                summary={movie.summary} 
+                genres={movie.genres} 
+              />
+            ))}
+          </div>
+          )} 
         </div>
-        )} 
       </div>
     );
 }
